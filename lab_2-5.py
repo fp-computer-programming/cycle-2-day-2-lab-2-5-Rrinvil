49e65f53de83d82a045ff9143aaaabb3a0166cc7
Rrinvil <144148135+Rrinvil@users.noreply.github.com>
2023-09-26 10:28:52 -0400

    Add files via upload

--- lab_2-5.py
+++ lab_2-5.py
@@ -5,10 +5,24 @@ w=21
 t=21
 a=21
 i=21
-#y = y-3
-# z **=2
-# w = w % 2
-# t = t/5
-# a *=12
-# i = i// 7
 # x += 5
+x=x+5
+print(x)
+#y = y-3
+y-= 3
+print(y)
+# z **=2
+z= z**2
+print(z)
+# w = w % 2
+w %=2
+print(w)
+# t = t/5
+t /=5
+print(t)
+# a *=12
+a= a*12
+print(a)
+# i = i// 7
+i = i// 7
+print(i)
